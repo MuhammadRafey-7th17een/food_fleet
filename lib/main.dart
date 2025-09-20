4c34c64cfd1ba4e5a7c7af2e1456953e7d94aa54
templi69 <f8.taha.amer@gmail.com>
2025-09-20 15:09:16 +0500

    taha

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_fleet/pages/log_in_page.dart';
-import 'package:food_fleet/pages/splash_screen_start.dart';
-import 'package:food_fleet/pages/storesignup.dart';
-
+import 'package:food_fleet/pages/signup_page.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -12,6 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home:LogInPage());
+
+    return const MaterialApp(home:SignUpPage());
+
+  
+
   }
 }
