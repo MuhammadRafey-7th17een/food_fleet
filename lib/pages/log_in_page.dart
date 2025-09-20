75eb8b05d773c6e3dbcb9ae6daf2d4b06a3d6f44
MuhammadRafey-7th17een <mrafey205@gmail.com>
2025-09-20 12:52:08 +0500

    appBar logo height/Width

--- lib/pages/log_in_page.dart
+++ lib/pages/log_in_page.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LogInPage extends StatelessWidget {
   const LogInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // gpt start
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.dark,
+      ),
+    );
+    //gpt end
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        toolbarHeight: 180,
+        backgroundColor: Colors.transparent,
+        title: Image.asset(
+          'assets/file_000000008fdc61faa669bc26c514dbc0 (1).png',
+          fit: BoxFit.contain,
+          width: 170,
+          height: 170,
+        ),
+      ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
@@ -16,6 +37,12 @@ class LogInPage extends StatelessWidget {
             colors: [Color(0xFF1A153A), Color(0XFF483AA0)],
             stops: [0.05, 0.37],
           ),
+        ),
+        child: Expanded(
+          child: Center(child: Column(children: [
+
+          ],
+        )),
         ),
       ),
     );
