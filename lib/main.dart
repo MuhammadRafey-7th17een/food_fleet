6bac655860a40802c81449b588b6f058f7cbbab6
MuhammadRafey-7th17een <mrafey205@gmail.com>
2025-09-22 17:03:21 +0500

    userHistoyUI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_fleet/pages/signup_page.dart';
+import 'package:food_fleet/pages/user_history.dart';
 import 'package:food_fleet/pages/user_signup.dart';
 
 void main() {
@@ -11,6 +12,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: UserSignup());
+    return const MaterialApp(home: UserHistory());
   }
 }
