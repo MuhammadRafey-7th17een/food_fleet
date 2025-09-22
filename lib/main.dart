4f49d17fc1954390f9b0170619019173df4717e3
MuhammadRafey-7th17een <mrafey205@gmail.com>
2025-09-22 17:08:43 +0500

    userHistory

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_fleet/pages/setting_page.dart';
 import 'package:food_fleet/pages/signup_page.dart';
+import 'package:food_fleet/pages/splash_screen_start.dart';
 import 'package:food_fleet/pages/user_history.dart';
 import 'package:food_fleet/pages/user_signup.dart';
 
@@ -13,7 +14,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: UserHistory());
-    return const MaterialApp(home: SettingsPage());
+    return const MaterialApp(home: StartSplashScreen());
   }
 }
