05b8ddea0b261fc3e3c1254cd01dd4b74003c98e
Basil132004 <basilkafayat@gmail.com>
2025-09-22 16:57:22 +0500

    user setting page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_fleet/pages/setting_page.dart';
 import 'package:food_fleet/pages/signup_page.dart';
 import 'package:food_fleet/pages/user_signup.dart';
 
@@ -11,6 +12,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: UserSignup());
+    return const MaterialApp(home: SettingsPage());
   }
 }
