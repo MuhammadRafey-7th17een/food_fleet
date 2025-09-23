3e5508cce5f8bebcd1def52d473f8e3e718846fd
templi69 <f8.taha.amer@gmail.com>
2025-09-23 16:03:33 +0500

    asas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'package:food_fleet/pages/User_homepage.dart';
-import 'package:food_fleet/pages/rider_settings.dart';
-import 'package:food_fleet/pages/setting_page.dart';
 import 'package:food_fleet/pages/signup_page.dart';
 import 'package:food_fleet/pages/splash_screen_start.dart';
 import 'package:food_fleet/pages/store_home_page.dart';
@@ -17,6 +16,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: RiderSettings());
+    return const MaterialApp(home: Userhomepage());
   }
 }
