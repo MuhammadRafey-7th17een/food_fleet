c0da10671fd3f2070ef1b3dd33ebf953d60f91f0
templi69 <f8.taha.amer@gmail.com>
2025-09-23 16:19:10 +0500

    4:29

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,17 @@ import 'package:food_fleet/pages/splash_screen_start.dart';
 import 'package:food_fleet/pages/store_home_page.dart';
 import 'package:food_fleet/pages/user_history.dart';
 import 'package:food_fleet/pages/user_signup.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MainApp());
+
+
+   WidgetsFlutterBinding.ensureInitialized();
+ Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MainApp extends StatelessWidget {
