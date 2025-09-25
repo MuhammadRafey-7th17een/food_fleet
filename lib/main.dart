99748f4994d18d57f63f3f8d8567184dbe6addd5
templi69 <f8.taha.amer@gmail.com>
2025-09-25 16:29:00 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home:UserSignup());
+    return const MaterialApp(home:SettingsPage());
   }
 }
