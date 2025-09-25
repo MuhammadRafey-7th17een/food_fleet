f0ff352f258ee6d1e9a098ba816274a83b8c77a0
Basil132004 <basilkafayat@gmail.com>
2025-09-25 13:56:19 +0500

    .

--- lib/pages/splash_screen_start.dart
+++ lib/pages/splash_screen_start.dart
@@ -1,13 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:food_fleet/pages/log_in_page.dart';
+import 'package:food_fleet/pages/rider_homepage.dart';
+import 'package:food_fleet/pages/signup_page.dart';
+import 'package:food_fleet/pages/store_home_page.dart';
+import 'package:food_fleet/pages/user_homepage.dart';
 
-class StartSplashScreen extends StatelessWidget {
+class StartSplashScreen extends StatefulWidget {
   const StartSplashScreen({super.key});
 
   @override
+  State<StartSplashScreen> createState() => _StartSplashScreenState();
+}
+
+class _StartSplashScreenState extends State<StartSplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkUser();
+  }
+
+  Future<void> _checkUser() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const SignUpPage()),
+      );
+    } else {
+      final doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+      
+      if (!doc.exists) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const SignUpPage()),
+        );
+        return;
+      }
+
+      final role = doc['role'];
+      if (role == "store") {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const StoreHomePage()),
+        );
+      } else if (role == "user") {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const Userhomepage()),
+        );
+      } else if (role == "rider") {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const Riderhomepage()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const SignUpPage()),
+        );
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // gpt start
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
@@ -15,13 +77,12 @@ class StartSplashScreen extends StatelessWidget {
         statusBarBrightness: Brightness.dark,
       ),
     );
-    //gpt end
 
     return Scaffold(
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
@@ -33,29 +94,20 @@ class StartSplashScreen extends StatelessWidget {
           children: [
             Expanded(
               child: Center(
-                child: Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    child: Image.asset(
-                      'assets/file_000000008fdc61faa669bc26c514dbc0 (1).png',
-                      width: 160,
-                      height: 160,
-                    ),
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LogInPage(),
-                        ),
-                      );
-                    },
-                  ),
+                child: Image.asset(
+                  'assets/file_000000008fdc61faa669bc26c514dbc0 (1).png',
+                  width: 160,
+                  height: 160,
                 ),
               ),
             ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 32.0),
+              child: CircularProgressIndicator(color: Colors.white),
+            ),
           ],
         ),
-      ),
-    );
-  }
+     ),
+);
+}
 }
