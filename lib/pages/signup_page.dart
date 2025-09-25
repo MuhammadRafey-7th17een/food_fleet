c57bb854f9d82d5b8e72e44683b8c8f1b2ad843a
Basil132004 <basilkafayat@gmail.com>
2025-09-25 14:31:06 +0500

    .

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -143,7 +143,7 @@ class SignUpPage extends StatelessWidget {
                     ),
                   ),
                   onPressed: () {
-                   Navigator.pushReplacement(
+                   Navigator.push(
                       context,
                       MaterialPageRoute(builder: (_) => const LogInPage()),
                     );
